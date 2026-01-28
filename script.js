0a21d0be344a74f7fe5a2e2de513cdf6ed802434
jj-jonix <idkwhyimmakinthis8@gmail.com>
2026-01-28 15:32:51 +0530

    navbar and partially done hamburger menu structure

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
- 
+ const hamburger = document.querySelector('.hamburger');
+ hamburger.onclick = () =>{
+    const navBar = document.querySelector('.navbar')
+    navBar.classList.toggle('active');
+ }
